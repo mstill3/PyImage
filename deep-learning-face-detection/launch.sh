8a12e8e60df79137fc74512343d13277c8c7fb8a
Matt Stillwell <stillwell006@gmail.com>
2018-06-25 20:20:02 +0000

    added launchers

---
+++ deep-learning-face-detection/launch.sh
@@ -0,0 +1,5 @@
+python detect_faces.py --image prom.jpg \
+                       --prototxt deploy.prototxt.txt \
+                       --model res10_300x300_ssd_iter_140000.caffemodel \
+                       --confidence 0.3
+
---
+++ object-detection-deep-learning/launch.sh
@@ -0,0 +1,4 @@
+python deep_learning_object_detection.py --image images/example_01.jpg \
+                                         --prototxt MobileNetSSD_deploy.prototxt.txt \
+                                         --model MobileNetSSD_deploy.caffemodel \
+                                         --confidence 0.3
